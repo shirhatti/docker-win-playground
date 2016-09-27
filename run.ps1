81c95b95680685c78c25c590c80333675dc49945
Sourabh Shirhatti <shirhatti@outlook.com>
2016-09-27 16:56:52 -0700

    Have run.ps1 trigger nuget restore

--- run.ps1
+++ run.ps1
@@ -18,5 +18,10 @@ function Invoke-MSBuild ([string]$MSBuildPath, [string]$MSBuildParameters) {
     Invoke-Expression "$MSBuildPath $MSBuildParameters"
 }
 
+function Invoke-NugetRestore ([string]$PackageConfigPath, [string]$OutputDirectory) {
+    Invoke-Expression "NuGet.exe install $PackageConfigPath -OutputDirectory $OutputDirectory"
+}
+
+Invoke-NugetRestore -PackageConfigPath ".\samples\HelloMVC\HelloMVC\packages.config" -OutputDirectory "samples\HelloMVC\packages"
 Invoke-MSBuild -MSBuildPath "MSBuild.exe" -MSBuildParameters ".\samples\HelloMVC\HelloMVC.sln /p:DeployOnBuild=true /p:PublishProfile=LocalFileSystem.pubxml"
 Invoke-Docker-Build -ImageName "hellomvc" -ImagePath ".\samples\HelloMVC\"
